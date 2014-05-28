9271d094604b9f3b4bcbd07d588ac9165cbb9eb2
fankejun <mac@Mac-Pro1.local>
2014-05-28 11:18:54 +0800

    Initial Commit

---
+++ IUTourism/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IUTourism
+//
+//  Created by mac on 14-5-28.
+//  Copyright (c) 2014年 frank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
